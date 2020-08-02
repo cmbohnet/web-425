d1021d0f97d802c1e5475706c5bdabd319b4efd2
Christine Bohnet <christine.bohnet@gmail.com>
2020-08-02 15:49:22 -0500

    week 4 assignments

---
+++ week-4/enterprise-composer-app/src/app/composer.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * Title: composer.interface.ts
+ * Author: Chris Bohnet
+ * Date: 31 July 2020
+ * Description: Interface for the Composer object
+ */
+
+export interface IComposer {
+  composerId: number;
+  fullName: string;
+  genre: string;
+}
---
+++ week-4/reactive-composer-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: app.component.ts
+ * Author: Chris Bohnet
+ * Date: 31 July 2020
+ * Description: App component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Enhanced Composer App';
+}
